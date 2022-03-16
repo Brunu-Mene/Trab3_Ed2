41d7633a12e6d2e9ac0a56250815c6c759336aab
Brunu-Mene <bruno.ufes@hotmail.com>
2022-03-16 11:27:19 -0300

    Finalizando

--- client/main.c
+++ client/main.c
@@ -9,32 +9,26 @@ int main(int argv, char ** args){
     preencheVector(vetOrdenado, args[1]);
     ordenaVetor(vetOrdenado);
     
-    filaPrioridade *fp = inicializaFP(vet);
     t_init = clock();
-    worstFit(fp,vet);
-    t_final = clock();
-    printf("Worst Fit: %d\n",getQtdDiscos(fp));
-    printf("Tempo gasto: %.3f (s)\n\n",(float)(t_final - t_init)/1000000);
 
+    filaPrioridade *fp = inicializaFP(vet);
+    worstFit(fp,vet);
+    printf("Worst Fit: %d\n",getQtdDiscos(fp));
+   
     filaPrioridade *fp2 = inicializaFP(vet);
-    t_init = clock();
     bestFit(fp2,vet);
-    t_final = clock();
     printf("Best Fit: %d\n",getQtdDiscos(fp2));
-    printf("Tempo gasto: %.3f (s)\n\n",(float)(t_final - t_init)/1000000);
+  
 
     filaPrioridade *fp3 = inicializaFP(vetOrdenado);
-    t_init = clock();
     worstFit(fp3,vetOrdenado);
-    t_final = clock();
     printf("Worst-Fit Decreasing: %d\n",getQtdDiscos(fp3));
-    printf("Tempo gasto: %.3f (s)\n\n",(float)(t_final - t_init)/1000000);
 
     filaPrioridade *fp4 = inicializaFP(vetOrdenado);
-    t_init = clock();
     bestFit(fp4,vetOrdenado);
-    t_final = clock();
     printf("Best-Fit Decreasing: %d\n",getQtdDiscos(fp4));
+
+    t_final = clock();
     printf("Tempo gasto: %.3f (s)\n\n",(float)(t_final - t_init)/1000000);
 
     liberaVector(vet);
